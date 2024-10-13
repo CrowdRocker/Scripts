174bd111a8cecb9f94d1892269394784f18e667b
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-13 12:45:22 -0500

    Update architec.sh

---
+++ architec.sh
@@ -0,0 +1,5 @@
+sudo pacman -S --needed git base-devel \
+  && git clone https://github.com/Gaming-Linux-FR/Architect.git ~/Architect \
+  && cd ~/Architect \
+  && chmod +x ./architect.sh \
+  && ./architect.sh
