c6c44ce0fdcbcc6cfb29d10f93bd7cc5d0da3f6c
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-14 11:27:54 -0500

    Update chaotic.sh

--- chaotic.sh
+++ chaotic.sh
@@ -8,5 +8,9 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+### cachyos-repo
+wget https://mirror.cachyos.org/cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo
+sudo ./cachyos-repo.sh
 
 sudo pacman -Syyu && sudo pacman -Syu
