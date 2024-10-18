f00b3de712f5303b44cb6ac25cd2b33fe357aedb
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-18 09:29:08 -0500

    Update swayfx.sh

--- swayfx.sh
+++ swayfx.sh
@@ -1,1 +1,7 @@
+# !/bin/bash
+
+
+
+###### swayfx ########
+
 yay -S --noconfirm --needed swayfx waybar nwg-panel nwg-launchers wofi mako avizo alacritty wf-recorder wl-clipboard swww swaybg wdisplay waypaper picom polykit nwg-look fuzzel qt5ct qt6ct alacritty kitty xsettingsd
