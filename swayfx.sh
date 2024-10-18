2a07c238eda7e70f503d24a15b7690c492f70380
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-18 09:47:12 -0500

    Update swayfx.sh

--- swayfx.sh
+++ swayfx.sh
@@ -5,6 +5,6 @@
 ###### swayfx ########
 
 yay -S --noconfirm --needed swayfx waybar nwg-panel nwg-launchers wofi mako avizo alacritty wf-recorder wl-clipboard swww swaybg wdisplay waypaper picom polykit nwg-look fuzzel qt5ct qt6ct alacritty kitty xsettingsd 
-yay -S --noconfirm --needed swaybg swayidle swaylock wofi wlroots wayland wayland-protocols light grim XWayland slurp wlr-randr wayland-utils
+yay -S --noconfirm --needed swaybg swayidle swaylock wofi wlroots wayland wayland-protocols light grim XWayland slurp wlr-randr wayland-utils google-chrome 
 
 sudo pacman -Syu
