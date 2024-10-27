1831273b5f1166b8136203f73f340d63fb61aa5a
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-27 02:25:34 -0500

    Update jakoolit.sh

--- jakoolit.sh
+++ jakoolit.sh
@@ -1,3 +1,19 @@
+#!/bin/sh
+############  Jakoolit hyprland  ###################
+# Jakoolit hyprland Linux Dotfiles
+
+
+sudo pacman -Sy archlinux-keyring && sudo pacman -Syyu
+
+# enable flatpaks
+sudo pacman -S --noconfirm --needed flatpak
+
+###### yay ########
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd
+
+
+
+
 git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
 cd ~/Arch-Hyprland
 chmod +x install.sh
