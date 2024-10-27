c7500c6cfd882a1cb8853535a5bb0ff70ff210cf
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-27 02:33:33 -0500

    Update ml4w.sh

---
+++ ml4w.sh
@@ -0,0 +1,13 @@
+
+
+
+
+
+#Main Release
+
+#You can use your preferred AUR helper.
+
+yay -S --noconfirm --needed ml4w-hyprland
+#After the installation, you can start the setup with
+
+ml4w-hyprland-setup
