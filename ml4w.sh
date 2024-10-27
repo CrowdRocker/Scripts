d26ea3e0d96adde2cda1c8f51dde7e38206f3904
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-27 02:36:11 -0500

    Update ml4w.sh

--- ml4w.sh
+++ ml4w.sh
@@ -1,6 +1,15 @@
+#!/bin/sh
+############  ml4w-hyprland-setup  ###################
+# ml4w-hyprland-setup Linux Dotfiles
 
 
+sudo pacman -Sy archlinux-keyring && sudo pacman -Syyu
 
+# enable flatpaks
+sudo pacman -S --noconfirm --needed flatpak
+
+###### yay ########
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd
 
 
 #Main Release
