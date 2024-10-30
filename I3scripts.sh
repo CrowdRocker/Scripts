0c2ef8dec9b540e31f3abee8eaf2cbb8ac051cbb
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-10-30 09:16:51 -0500

    Update I3scripts.sh

--- I3scripts.sh
+++ I3scripts.sh
@@ -1,5 +1,7 @@
-# !/bin/bash
-
+#!/bin/sh
+############  yurihikari garuda-hyprdots ###################
+# Yurihikari's Garuda Linux Dotfiles
+# My Garuda Sway Config (Hyprland + Sway)
 
 sudo pacman -Sy archlinux-keyring && sudo pacman -Syyu
 
@@ -12,7 +14,20 @@ sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/ya
 ###### paru ########
 yay -S --noconfirm --needed paru 
 
-###### i3 #######
+
+paru -S swaylock-effects hyprlock-git rofi-wayland waybar fastfetch cava-git foot hyprland-git mpd mpc rose-pine-cursor rose-pine-hyprcursor ttf-font-awesome nerd-fonts hyprpicker pipewire wireplumber nwg-launchers mako most pavucontrol swayfx bluez bluez-utils grimblast gpu-screen-recorder btop networkmanager matugen wl-clipboard swww dart-sass brightnessctl gnome-bluetooth-3.0 aylurs-gtk-shell micro blueberry kitty
+
+
+############  Installation ###################
+git clone https://github.com/yurihikari/garuda-sway-config.git
+cp -r garuda-sway-config/* ~/.config
+
+
+############  Install Script ###################
+git clone https://github.com/yurihikari/garuda-sway-config.git
+cd ./garuda-sway-config
+./install.sh
+# You'll have some interactions sometime, it's not fully automatized
 
 
 # sddm
