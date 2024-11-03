a467d3a90283ed29ab412263c5806ae6e962467d
Matthew B. <131808292+CrowdRocker@users.noreply.github.com>
2024-11-02 21:23:08 -0500

    Update chaotic.sh

--- chaotic.sh
+++ chaotic.sh
@@ -8,6 +8,28 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+
+# Add chaotic AUR keys and repository if not added, else skip
+echo "Adding chaotic AUR keys and repository"
+if ! pacman-key --list-keys chaotic-aur &> /dev/null; then
+  echo "Adding chaotic AUR keys and repository"
+  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+  sudo pacman-key --lsign-key 3056513887B78AEB
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+  echo "Chaotic AUR keys added successfully"
+else 
+  echo "Chaotic AUR keys and repository already added. Skipping..."
+fi
+
+# Add chaotic AUR
+if ! grep -q "^\[chaotic-aur\]" /etc/pacman.conf; then
+  echo "[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+  sudo pacman -Sy
+  echo "Chaotic AUR repository added/updated successfully"
+fi
+
 ### cachyos-repo
 wget https://mirror.cachyos.org/cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
